5e83efe056f9bed8c0edcbd7eb89020c245533c4
tonyp <tonyptonyp@gmail.com>
2013-12-04 02:25:46 +0100

    initial Android 4.4 bringup
    - update all apks & libs to 4.4 ones
    - adapt new folder structure
    - remove gtalk (hangouts should be optimized for lowram devices now)

--- tiny_gapps_nonneon_tonyp.mk
+++ tiny_gapps_nonneon_tonyp.mk
@@ -4,23 +4,24 @@ ifeq ($(MINI_GAPPS),true)
  # install
 PRODUCT_COPY_FILES += \
     vendor/google/system/app/ChromeBookmarksSyncAdapter.apk:system/app/ChromeBookmarksSyncAdapter.apk \
-    vendor/google/system/app/GoogleBackupTransport.apk:system/app/GoogleBackupTransport.apk \
+    vendor/google/system/priv-app/GoogleBackupTransport.apk:system/priv-app/GoogleBackupTransport.apk \
     vendor/google/system/app/GoogleCalendarSyncAdapter.apk:system/app/GoogleCalendarSyncAdapter.apk \
     vendor/google/system/app/GoogleContactsSyncAdapter.apk:system/app/GoogleContactsSyncAdapter.apk \
-    vendor/google/system/app/GoogleFeedback.apk:system/app/GoogleFeedback.apk \
-    vendor/google/system/app/GoogleLoginService.apk:system/app/GoogleLoginService.apk \
-    vendor/google/system/app/GoogleMaps.apk:system/app/GoogleMaps.apk \
-    vendor/google/system/app/GoogleOneTimeInitializer.apk:system/app/GoogleOneTimeInitializer.apk \
-    vendor/google/system/app/GooglePartnerSetup.apk:system/app/GooglePartnerSetup.apk \
-    vendor/google/system/app/GoogleServicesFramework.apk:system/app/GoogleServicesFramework.apk \
-    vendor/google/system/app/GoogleTTS.apk:system/app/GoogleTTS.apk \
-    vendor/google/system/app/LatinIME.apk:system/app/LatinIME.apk \
+    vendor/google/system/priv-app/GmsCore.apk:system/priv-app/GmsCore.apk \
+    vendor/google/system/priv-app/GoogleFeedback.apk:system/priv-app/GoogleFeedback.apk \
+    vendor/google/system/priv-app/GoogleLoginService.apk:system/priv-app/GoogleLoginService.apk \
+    vendor/google/system/priv-app/GoogleMaps.apk:system/priv-app/GoogleMaps.apk \
+    vendor/google/system/priv-app/GoogleOneTimeInitializer.apk:system/priv-app/GoogleOneTimeInitializer.apk \
+    vendor/google/system/priv-app/GooglePartnerSetup.apk:system/priv-app/GooglePartnerSetup.apk \
+    vendor/google/system/priv-app/GoogleServicesFramework.apk:system/priv-app/GoogleServicesFramework.apk \
+    vendor/google/system/priv-app/GoogleTTS.apk:system/priv-app/GoogleTTS.apk \
+    vendor/google/system/priv-app/LatinIME.apk:system/priv-app/LatinIME.apk \
     vendor/google/system/app/MediaUploader.apk:system/app/MediaUploader.apk \
-    vendor/google/system/app/NetworkLocation.apk:system/app/NetworkLocation.apk \
-    vendor/google/system/app/Phonesky.apk:system/app/Phonesky.apk \
-    vendor/google/system/app/Talk.apk:system/app/Talk.apk
+    vendor/google/system/priv-app/Phonesky.apk:system/priv-app/Phonesky.apk \
+    vendor/google/system/priv-app/SetupWizard.apk:system/priv-app/SetupWizard.apk
 
 PRODUCT_COPY_FILES += \
+    vendor/google/system/etc/preferred-apps/google.xml:system/etc/preferred-apps/google.xml \
     vendor/google/system/etc/permissions/com.google.android.maps.xml:system/etc/permissions/com.google.android.maps.xml \
     vendor/google/system/etc/permissions/com.google.android.media.effects.xml:system/etc/permissions/com.google.android.media.effects.xml \
     vendor/google/system/etc/permissions/com.google.widevine.software.drm.xml:system/etc/permissions/com.google.widevine.software.drm.xml \
@@ -32,37 +33,31 @@ PRODUCT_COPY_FILES += \
     vendor/google/system/framework/com.google.widevine.software.drm.jar:system/framework/com.google.widevine.software.drm.jar
 
 PRODUCT_COPY_FILES += \
-    vendor/google/system/lib/libgtalk_jni.so:system/lib/libgtalk_jni.so \
-    vendor/google/system/lib/libjni_t13n_shared_engine.so:system/lib/libjni_t13n_shared_engine.so \
-    vendor/google/system/lib/libjni_latinimegoogle.so:system/lib/libjni_latinimegoogle.so \
-    vendor/google/system/lib/libpatts_engine_jni_api.so:system/lib/libpatts_engine_jni_api.so \
-    vendor/google/system/lib/libspeexwrapper.so:system/lib/libspeexwrapper.so \
-    vendor/google/system/lib/libvideochat_stabilize.so:system/lib/libvideochat_stabilize.so \
-    vendor/google/system/lib/libwebrtc_audio_coding.so:system/lib/libwebrtc_audio_coding.so
-
-#temporary hack: ensure the SetupWizard gets deleted:
-$(shell rm -f $(OUT)/system/app/SetupWizard.apk)
+    vendor/google/system/lib/libAppDataSearch.so:system/lib/libAppDataSearch.so \
+    vendor/google/system/lib/libgames_rtmp_jni.so:system/lib/libgames_rtmp_jni.so \
+    vendor/google/system/lib/libjni_unbundled_latinimegoogle.so:system/lib/libjni_unbundled_latinimegoogle.so \
+    vendor/google/system/lib/libspeexwrapper.so:system/lib/libspeexwrapper.so
 
 else
 # uninstall
 $(shell rm -f $(OUT)/system/app/ChromeBookmarksSyncAdapter.apk)
-$(shell rm -f $(OUT)/system/app/GoogleBackupTransport.apk)
+$(shell rm -f $(OUT)/system/priv-app/GoogleBackupTransport.apk)
 $(shell rm -f $(OUT)/system/app/GoogleCalendarSyncAdapter.apk)
 $(shell rm -f $(OUT)/system/app/GoogleContactsSyncAdapter.apk)
-$(shell rm -f $(OUT)/system/app/GoogleFeedback.apk)
-$(shell rm -f $(OUT)/system/app/GoogleLoginService.apk)
-$(shell rm -f $(OUT)/system/app/GoogleMaps.apk)
-$(shell rm -f $(OUT)/system/app/GoogleOneTimeInitializer.apk)
-$(shell rm -f $(OUT)/system/app/GooglePartnerSetup.apk)
-$(shell rm -f $(OUT)/system/app/GoogleServicesFramework.apk)
-$(shell rm -f $(OUT)/system/app/GoogleTTS.apk)
-$(shell rm -f $(OUT)/system/app/LatinIME.apk)
+$(shell rm -f $(OUT)/system/priv-app/GmsCore.apk)
+$(shell rm -f $(OUT)/system/priv-app/GoogleFeedback.apk)
+$(shell rm -f $(OUT)/system/priv-app/GoogleLoginService.apk)
+$(shell rm -f $(OUT)/system/priv-app/GoogleMaps.apk)
+$(shell rm -f $(OUT)/system/priv-app/GoogleOneTimeInitializer.apk)
+$(shell rm -f $(OUT)/system/priv-app/GooglePartnerSetup.apk)
+$(shell rm -f $(OUT)/system/priv-app/GoogleServicesFramework.apk)
+$(shell rm -f $(OUT)/system/priv-app/GoogleTTS.apk)
+$(shell rm -f $(OUT)/system/priv-app/LatinIME.apk)
 $(shell rm -f $(OUT)/system/app/MediaUploader.apk)
-$(shell rm -f $(OUT)/system/app/NetworkLocation.apk)
-$(shell rm -f $(OUT)/system/app/Phonesky.apk)
-$(shell rm -f $(OUT)/system/app/SetupWizard.apk)
-$(shell rm -f $(OUT)/system/app/Talk.apk)
+$(shell rm -f $(OUT)/system/priv-app/Phonesky.apk)
+$(shell rm -f $(OUT)/system/priv-app/SetupWizard.apk)
 
+$(shell rm -f $(OUT)/system/etc/preferred-apps/google.xml)
 $(shell rm -f $(OUT)/system/etc/permissions/com.google.android.maps.xml)
 $(shell rm -f $(OUT)/system/etc/permissions/com.google.android.media.effects.xml)
 $(shell rm -f $(OUT)/system/etc/permissions/com.google.widevine.software.drm.xml)
@@ -72,12 +67,9 @@ $(shell rm -f $(OUT)/system/framework/com.google.android.maps.jar)
 $(shell rm -f $(OUT)/system/framework/system/framework/com.google.android.media.effects.jar)
 $(shell rm -f $(OUT)/system/framework/com.google.widevine.software.drm.jar)
 
-$(shell rm -f $(OUT)/system/lib/libgtalk_jni.so)
-$(shell rm -f $(OUT)/system/lib/libjni_t13n_shared_engine.so)
-$(shell rm -f $(OUT)/system/lib/libjni_latinimegoogle.so)
-$(shell rm -f $(OUT)/system/lib/libpatts_engine_jni_api.so)
+$(shell rm -f $(OUT)/system/lib/libAppDataSearch.so)
+$(shell rm -f $(OUT)/system/lib/libgames_rtmp_jni.so)
+$(shell rm -f $(OUT)/system/lib/libjni_unbundled_latinimegoogle.so)
 $(shell rm -f $(OUT)/system/lib/libspeexwrapper.so)
-$(shell rm -f $(OUT)/system/lib/libvideochat_stabilize.so)
-$(shell rm -f $(OUT)/system/lib/libwebrtc_audio_coding.so)
 
 endif
